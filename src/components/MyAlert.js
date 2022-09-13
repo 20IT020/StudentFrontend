4c30e975212670f0f46babac09ddf7c10e5b5d01
Yash Dalia <20it020@charusat.edu.in>
2022-09-13 13:07:55 +0530

    second commit

--- src/components/MyAlert.js
+++ src/components/MyAlert.js
@@ -3,6 +3,7 @@ import { Alert } from "react-bootstrap";
 
 export default function MyAlert(props) {
   return (
+//    props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
     props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
   );
 }
